9b340f7c8a91ae350916d9cec6570b1a668972aa
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2023-02-08 17:52:54 +0200

    add adaptive tablet and desktop

--- src/assets/js/slickBuy.js
+++ src/assets/js/slickBuy.js
@@ -1,19 +1,20 @@
 import $ from 'jquery';
 import 'slick-carousel';
 
-window.jQuery = window.$ = $;
-
 $('.buy__slider-image').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
+  mobileFirst: true,
   arrows: true,
   fade: true,
   asNavFor: '.buy__slider-nav',
 });
 $('.buy__slider-nav').slick({
-  slidesToShow: 3,
+  slidesToShow: 4,
   slidesToScroll: 1,
+  mobileFirst: true,
   asNavFor: '.buy__slider-image',
   dots: false,
   arrows: false,
+  focusOnSelect: true,
 });

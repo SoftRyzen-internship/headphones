a421b3d0a157db8e12f79bf0eb5f350bff5f8991
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2023-02-08 09:13:36 +0200

    FE-9: change file slickBuy.js

--- src/assets/js/slickBuy.js
+++ src/assets/js/slickBuy.js
@@ -3,21 +3,19 @@ import 'slick-carousel';
 
 window.jQuery = window.$ = $;
 
-$(document).ready(function () {
-  $('.buy__slider-image').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    fade: true,
-    asNavFor: '.buy__slider-nav',
-  });
-  $('.buy__slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.buy__slider-image',
-    dots: false,
-    arrows: false,
-    centerMode: false,
-    focusOnSelect: true,
-  });
+$('.buy__slider-image').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  fade: true,
+  asNavFor: '.buy__slider-nav',
+});
+$('.buy__slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.buy__slider-image',
+  dots: false,
+  arrows: false,
+  centerMode: false,
+  focusOnSelect: true,
 });

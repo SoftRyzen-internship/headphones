298346be65b428eaf9c529c8bc85e822ffb9274c
Vita Kostiuk <vitagrebennik@gmail.com>
2023-02-14 13:15:47 +0200

    FE-5:change-appearance-popup-and-fix-disabling-btn

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -4,6 +4,154 @@ const sendBtn = document.querySelector('.send__btn');
 const username = document.getElementById('name');
 const phone = document.getElementById('telephone');
 
+const modal = document.querySelector('[data-modal]');
+
+// SET DISABLED BUTTON
+sendBtn.setAttribute('disabled', 'disabled');
+
+const isValidValues = {
+  name: false,
+  phone: false,
+};
+
+// SET DISABLED BUTTON
+modal.addEventListener('click', () => {
+  sendBtn.setAttribute('disabled', 'disabled');
+});
+
+// скидаємо значення isValidValues
+sendBtn.addEventListener('click', () => {
+  isValidValues.name = false;
+  isValidValues.phone = false;
+});
+
+form.addEventListener('input', e => {
+  if (e.target.id === 'name') {
+    const isValidName = checkInputName(e.target.value);
+
+    if (isValidName) {
+      isValidValues.name = true;
+    }
+    if (!isValidName) {
+      isValidValues.name = false;
+      sendBtn.setAttribute('disabled', 'disabled');
+    }
+  }
+
+  if (e.target.id === 'telephone') {
+    const isValidPhone = checkInputPhone(e.target.value);
+
+    if (isValidPhone) {
+      isValidValues.phone = true;
+    }
+    if (!isValidPhone) {
+      isValidValues.phone = false;
+      sendBtn.setAttribute('disabled', 'disabled');
+    }
+  }
+
+  console.log('isValidValues', isValidValues);
+
+  // REMOVE DISABLED BUTTON
+  if (isValidValues.name === true && isValidValues.phone === true) {
+    sendBtn.removeAttribute('disabled');
+  }
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // REMOVE DISABLED BUTTON
+  if (isValidValues.name === true && isValidValues.phone === true) {
+    e.currentTarget.reset();
+  }
+});
+
+// CHECK INPUT NAME
+const checkInputName = name => {
+  let isValidName = false;
+
+  // Get values from the inputs
+  const usernameValue = name.trim();
+
+  // VALIDATION NAME
+  const nameRe = /^[a-zA-Z-]+$/;
+  if (!usernameValue) {
+    //Show error
+    //Add error class
+    setErrorFor(username, 'Error(field is required)');
+  } else if (usernameValue.length < 2) {
+    setErrorFor(username, 'Error(from 2 to 30 letters)');
+  } else if (usernameValue.length > 30) {
+    setErrorFor(username, 'Error(from 2 to 30 letters)');
+  } else if (!usernameValue.match(nameRe)) {
+    //Show error
+    //Add error class
+    setErrorFor(username, 'Error(only latin letters)');
+  } else {
+    //Add succes class
+    setSuccessFor(username);
+    isValidName = true;
+  }
+
+  return isValidName;
+};
+
+// CHECK INPUT PHONE
+const checkInputPhone = phoneInput => {
+  let isValidPhone = false;
+  // Get values from the inputs
+  const phoneValue = phoneInput.trim();
+
+  // VALIDATION PHONE
+  const re = /^[0-9+-]+$/;
+  if (!phoneValue) {
+    //Show error
+    //Add error class
+    setErrorFor(phone, 'Error(field is required)');
+  } else if (!phoneValue.startsWith('+44')) {
+    setErrorFor(phone, 'Error(start with "+44)');
+  } else if (phoneValue.length < 13 || phoneValue.length >= 14) {
+    setErrorFor(phone, 'Error(only 12 numbers)');
+  } else if (!phoneValue.match(re)) {
+    //Add error class
+    setErrorFor(phone, 'Error(only numbers)');
+  } else {
+    //Add succes class
+    setSuccessFor(phone);
+    isValidPhone = true;
+  }
+
+  return isValidPhone;
+};
+
+// SET CLASSNAME FOR ERROR
+const setErrorFor = (input, message) => {
+  const formControl = input.parentElement;
+  const errorText = formControl.querySelector('span');
+
+  //add error message inside small
+  errorText.innerText = message;
+
+  //add error class
+  formControl.className = 'form-control error';
+};
+
+// SET CLASSNAME FOR SUCCESS
+const setSuccessFor = input => {
+  const formControl = input.parentElement;
+
+  //add success class
+  formControl.className = 'form-control success';
+};
+
+/*
+const form = document.getElementById('form');
+const sendBtn = document.querySelector('.send__btn');
+
+const username = document.getElementById('name');
+const phone = document.getElementById('telephone');
+
 // SET DISABLED BUTTON
 sendBtn.setAttribute('disabled', 'disabled');
 
@@ -129,3 +277,4 @@ const setSuccessFor = input => {
   //add success class
   formControl.className = 'form-control success';
 };
+*/
--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -3,6 +3,92 @@ import $ from 'jquery';
 $(document).ready(function () {
   const openModalBtn = document.querySelectorAll('.buy__btn');
   const modal = document.querySelector('[data-modal]');
+  const modalWindow = document.querySelector('.modal');
+  const closeModalBtn = document.querySelector('[data-modal-close]');
+  const body = document.querySelector('body');
+  const backdrop = document.querySelector('.backdrop');
+  const sendBtn = document.querySelector('.send__btn');
+  const popup = document.querySelector('.popup');
+
+  const form = document.getElementById('form');
+  const username = document.getElementById('name');
+  const phone = document.getElementById('telephone');
+
+  openModalBtn.forEach(function (btn) {
+    btn.addEventListener('click', e => {
+      e.preventDefault();
+
+      //remove values in form
+      form[0].value = '';
+      form[1].value = '';
+      //remove success and error classes
+      username.parentElement.classList.remove('success');
+      username.parentElement.classList.remove('error');
+      phone.parentElement.classList.remove('success');
+      phone.parentElement.classList.remove('error');
+
+      toggleModal();
+    });
+  });
+
+  function toggleModal() {
+    modal.classList.toggle('is-hidden');
+    modalWindow.classList.toggle('is-hidden');
+    body.classList.toggle('scroll-hidden');
+  }
+
+  function togglePopup() {
+    popup.classList.toggle('is-hidden');
+    body.classList.toggle('scroll-hidden');
+  }
+
+  function handleKey(e) {
+    if (
+      !modal.classList.contains('is-hidden') &&
+      !modalWindow.classList.contains('is-hidden') &&
+      popup.classList.contains('is-hidden')
+    ) {
+      if (e.key === 'Escape') {
+        toggleModal();
+      }
+    }
+
+    if (
+      !modal.classList.contains('is-hidden') &&
+      modalWindow.classList.contains('is-hidden') &&
+      !popup.classList.contains('is-hidden')
+    ) {
+      if (e.key === 'Escape') {
+        modal.classList.add('is-hidden');
+        togglePopup();
+      }
+    }
+    return;
+  }
+
+  function handleClose(e) {
+    if (e.target === e.currentTarget && popup.classList.contains('is-hidden')) {
+      toggleModal();
+    }
+
+    if (e.target === e.currentTarget && !popup.classList.contains('is-hidden')) {
+      modal.classList.add('is-hidden');
+      togglePopup();
+    }
+    return;
+  }
+
+  document.addEventListener('keydown', handleKey);
+  backdrop.addEventListener('mousedown', handleClose);
+  closeModalBtn.addEventListener('click', toggleModal);
+});
+
+/*
+import $ from 'jquery';
+
+$(document).ready(function () {
+  const openModalBtn = document.querySelectorAll('.buy__btn');
+  const modal = document.querySelector('[data-modal]');
   const closeModalBtn = document.querySelector('[data-modal-close]');
   const body = document.querySelector('body');
   const backdrop = document.querySelector('.backdrop');
@@ -56,3 +142,4 @@ $(document).ready(function () {
   closeModalBtn.addEventListener('click', toggleModal);
   sendBtn.addEventListener('click', toggleModal);
 });
+*/

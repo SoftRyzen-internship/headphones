f11de70eeb74efe8184da178ef37c7476fbf035e
Vita Kostiuk <vitagrebennik@gmail.com>
2023-02-15 12:19:24 +0200

    fix-css-remove-commented-code

--- src/assets/js/popup.js
+++ src/assets/js/popup.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
 
   openPopupBtn.forEach(function (btn) {
     btn.addEventListener('click', e => {
-      console.log('click on send btn');
       e.preventDefault();
 
       modalWindow.classList.toggle('is-hidden');
@@ -31,49 +30,3 @@ $(document).ready(function () {
 
   closePopupBtn.addEventListener('click', closePopup);
 });
-
-//+444444444444
-
-// import $ from 'jquery';
-
-// $(document).ready(function () {
-//   const openPopupBtn = document.querySelectorAll('.send__btn');
-//   const popup = document.querySelector('[data-popup]');
-//   const closePopupBtn = document.querySelector('[data-popup-close]');
-//   const body = document.querySelector('body');
-//   const backdrop = document.querySelector('.backdrop-popup');
-
-//   openPopupBtn.forEach(function (btn) {
-//     btn.addEventListener('click', e => {
-//       e.preventDefault();
-//       // setTimeout(() => {
-//       toggleModal();
-//       // }, 500);
-//     });
-//   });
-
-// function toggleModal() {
-//   popup.classList.toggle('is-hidden');
-//   body.classList.toggle('scroll-hidden');
-// }
-
-//   function handleKey(e) {
-// if (!popup.classList.contains('is-hidden')) {
-//   if (e.key === 'Escape') {
-//     toggleModal();
-//   }
-// }
-//     return;
-//   }
-
-//   function handleClose(e) {
-//     if (e.target === e.currentTarget) {
-//       toggleModal();
-//     }
-//     return;
-//   }
-
-//   document.addEventListener('keydown', handleKey);
-//   backdrop.addEventListener('mousedown', handleClose);
-//   closePopupBtn.addEventListener('click', toggleModal);
-// });
